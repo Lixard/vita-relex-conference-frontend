5b7cd80187996a620c7927df7a3d96110048f8f3
Vlad Peltikhin <peltikhin.vladik@gmail.com>
2020-03-26 00:12:28 +0300

    Add new routing and new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,18 @@ import {AppComponent} from './app.component';
 import {MainPageComponent} from './pages/main-page/main-page.component';
 import {ConferencesListPageComponent} from './pages/conferences-list-page/conferences-list-page.component';
 import {AboutConferencePageComponent} from './pages/about-conference-page/about-conference-page.component';
+import {AboutEventPageComponent} from './pages/about-event-page/about-event-page.component';
 
 
 const routes: Routes = [
   {
     path: 'events',
-    component: EventsListPageComponent
+    component: EventsListPageComponent,
+    children: [
+      {
+        path: 'about/:event',
+        component: AboutEventPageComponent,
+      }]
   },
   {
     path: 'conferences',
@@ -26,6 +32,14 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     component: MainPageComponent
+  },
+  {
+    path: 'about-conference/:conference',
+    component: AboutConferencePageComponent,
+  },
+  {
+    path: 'about-event/:event',
+    component: AboutEventPageComponent,
   },
 ];
 
--- src/app/pages/about-event-page/about-event-page.component.ts
+++ src/app/pages/about-event-page/about-event-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Conference} from '../conferences-list-page/Conference';
+import {ActivatedRoute} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {Event} from '../events-list-page/Event';
 
 @Component({
   selector: 'app-about-event-page',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-event-page.component.scss']
 })
 export class AboutEventPageComponent implements OnInit {
+  event: Event;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private httpClient: HttpClient) { }
 
   ngOnInit(): void {
-  }
+    const eventId = this.route.snapshot.paramMap.get('event');
+    this.httpClient.get<Event>(`http://localhost:8080/events/${eventId}`).subscribe(event => {
+      this.event = event;
+    });
 
+  }
 }
