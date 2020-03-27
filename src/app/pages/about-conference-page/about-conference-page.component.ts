1fdd138f76b660dbcb504dfc3f79b60234a00d33
Vlad Peltikhin <peltikhin.vladik@gmail.com>
2020-03-27 23:07:01 +0300

    Replace event-list-of-conference=page functional in about-conference-page

--- src/app/pages/about-conference-page/about-conference-page.component.ts
+++ src/app/pages/about-conference-page/about-conference-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import {ConferenceModel} from '../../models/conference.model';
+import {EventModel} from '../../models/event.model';
 
 @Component({
   selector: 'app-about-conference-page',
@@ -12,12 +13,17 @@ export class AboutConferencePageComponent implements OnInit {
 
   conference: ConferenceModel;
 
+  events: EventModel[];
+
   constructor(private route: ActivatedRoute, private httpClient: HttpClient) { }
 
   ngOnInit(): void {
     const conferenceId = this.route.snapshot.paramMap.get('conference');
     this.httpClient.get<ConferenceModel>(`http://localhost:8080/conferences/${conferenceId}`).subscribe(conference => {
       this.conference = conference;
+    });
+    this.httpClient.get<EventModel[]>(`http://localhost:8080/conferences/${conferenceId}/events`).subscribe(events => {
+      this.events = events;
     });
 
   }
