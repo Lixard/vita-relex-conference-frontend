eb89e590533d5989e4bd9ca672c2c438a8d302cc
Vlad Peltikhin <peltikhin.vladik@gmail.com>
2020-03-30 00:01:47 +0300

    Fix datetime

--- src/app/components/event-editor/event-editor.component.ts
+++ src/app/components/event-editor/event-editor.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {EventCreateForm, EventCreateModel, EventModel} from '../../models/event.model';
 import {EventService} from '../../services/event.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-event-editor',
@@ -40,13 +41,16 @@ export class EventEditorComponent implements OnInit {
         timeEnd:  this.formBuilder.control(undefined, [ Validators.required]),
       });
     } else {
+      // TODO check it
+      const timeStart = moment(this.event.details.timeStart).format('YYYY-MM-DDTHH:mm');
+      const timeEnd = moment(this.event.details.timeEnd).format('YYYY-MM-DDTHH:mm');
       this.form = this.formBuilder.group({
       eventName: this.formBuilder.control(this.event.eventName , [Validators.max(50), Validators.required]),
       eventType: this.formBuilder.control(this.event.eventType, [Validators.required]),
       htmlDescription:  this.formBuilder.control(this.event.details.htmlDescription, [Validators.required]),
       location:  this.formBuilder.control(this.event.details.location, [Validators.max(50), Validators.required]),
-      timeStart:  this.formBuilder.control(this.event.details.timeStart, [ Validators.required]),
-      timeEnd:  this.formBuilder.control(this.event.details.timeEnd, [ Validators.required]),
+      timeStart:  this.formBuilder.control(timeStart, [ Validators.required]),
+      timeEnd:  this.formBuilder.control(timeEnd, [ Validators.required]),
     });
     }
   }
@@ -59,8 +63,8 @@ export class EventEditorComponent implements OnInit {
       details:   {
         htmlDescription: value.htmlDescription,
         location: value.location,
-        timeStart: value.timeStart,
-        timeEnd: value.timeEnd,
+        timeStart: moment(value.timeStart, 'YYYY-MM-DDTHH:mm').toISOString(),
+        timeEnd: moment(value.timeEnd, 'YYYY-MM-DDTHH:mm').toISOString(),
         createdBy: 1
         // TODO: insert user id when we add auth
       }};
