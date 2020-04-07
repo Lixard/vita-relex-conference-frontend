992e3e2c58fd6ae06417c69727702fccbe188b5e
Vlad Peltikhin <peltikhin.vladik@gmail.com>
2020-04-07 17:16:13 +0300

    Update auth service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { UserMenuComponent } from './components/user-menu/user-menu.component';
 import {MatButtonModule} from '@angular/material/button';
 import {RouterModule} from '@angular/router';
+import {LOAD_CURRENT_USER_INITIALIZER} from './services/auth.service';
 
 
 
@@ -14,6 +15,7 @@ import {RouterModule} from '@angular/router';
     RouterModule
   ],
   providers: [
+    LOAD_CURRENT_USER_INITIALIZER
   ],
   exports: [
     UserMenuComponent
