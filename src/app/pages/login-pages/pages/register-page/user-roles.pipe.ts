f317e387c528328a75808ef7cb895c892d55e1d4
Maksim Borisov <Lixard870@gmail.com>
2020-04-17 02:33:15 +0300

    Add userRoles pipe

---
+++ src/app/pages/login-pages/pages/register-page/user-roles.pipe.ts
@@ -0,0 +1,22 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {RegisterRoles} from '../../../../entities/user/models/user.model';
+
+@Pipe({
+  name: 'userRoles'
+})
+export class UserRolesPipe implements PipeTransform {
+
+  transform(value: RegisterRoles): string | undefined {
+    if (value === null) {
+      return undefined;
+    }
+    switch (value) {
+      case RegisterRoles.USER:
+        return 'Пользователь';
+      case RegisterRoles.COMPANY_ACCOUNT:
+        return 'Представитель компании';
+
+    }
+  }
+
+}
