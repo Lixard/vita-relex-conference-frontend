b516668fc3a8d12a01ec215d284216fe2c2ea60e
Maksim Borisov <Lixard870@gmail.com>
2020-04-19 18:46:33 +0300

    Add logging and routing to user profile after registration

--- src/app/pages/login-pages/pages/register-page/register-page.component.ts
+++ src/app/pages/login-pages/pages/register-page/register-page.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {RegisterService} from '../../../../core/services/register.service';
 import {RegisterRoles, UserModel, UserRegistrationForm} from '../../../../entities/user/models/user.model';
+import {Router} from '@angular/router';
+import {AuthService} from '../../../../core/services/auth.service';
+import {switchMap} from 'rxjs/operators';
+import {CurrentUserService} from '../../../../core/services/current-user.service';
 
 @Component({
   selector: 'app-register-page',
@@ -13,9 +17,13 @@ export class RegisterPageComponent implements OnInit {
   form: FormGroup;
   user: UserModel;
   readonly roles = [RegisterRoles.USER, RegisterRoles.COMPANY_ACCOUNT];
-  hide = true;
+  hidePassword = true;
 
-  constructor(private fb: FormBuilder, private registerService: RegisterService) {
+  constructor(private fb: FormBuilder,
+              private registerService: RegisterService,
+              private auth: AuthService,
+              private router: Router,
+              private currentUser: CurrentUserService) {
     this.buildForm();
   }
 
@@ -30,22 +38,32 @@ export class RegisterPageComponent implements OnInit {
       firstName: this.fb.control('', Validators.required),
       lastName: this.fb.control('', Validators.required),
       email: this.fb.control('', [Validators.required, Validators.email]),
-      role: this.fb.control(undefined, Validators.required)
+      role: this.fb.control(undefined)
     });
   }
 
   register(form: UserRegistrationForm) {
-       this.user = {
-         username: form.username,
-         password: form.password,
-         role: form.role,
-         personalInfo: {
-           firstName: form.firstName,
-           lastName: form.lastName,
-           email: form.email
-         }
-       } as unknown as UserModel;
-       this.registerService.registerUser(this.user).subscribe(() => console.log('success!'));
+    this.user = {
+      username: form.username,
+      password: form.password,
+      role: form.role,
+      personalInfo: {
+        firstName: form.firstName,
+        lastName: form.lastName,
+        email: form.email
+      }
+    } as unknown as UserModel;
+    this.registerService.registerUser(this.user).subscribe(() => {
+      this.auth.login({
+        username: form.username,
+        password: form.password
+      }).pipe(switchMap(() => this.auth.loadProfile())).subscribe(() => {
+        this.auth.loadProfile().subscribe(
+          (profile) => {
+            this.currentUser.user$.next(profile);
+            this.router.navigateByUrl(`/users/${profile.id}`);
+          });
+      });
+    });
   }
 }
-
